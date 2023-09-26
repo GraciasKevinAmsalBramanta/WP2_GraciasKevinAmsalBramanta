a5b74ea93af8ca9ac6ffdf5b5491016650ae96ed
GraciasKevinAmsalBramanta <graciaskevin99@gmail.com>
2023-09-26 19:46:01 +0700

    Update Tugas Pertemuan 2

---
+++ application/views/view-form-matakuliah.php
@@ -0,0 +1,52 @@
+<html>
+
+<head>
+    <title>Form Input Matakuliah</title>
+</head>
+
+<body>
+    <center>
+        <form action="<?= base_url('matakuliah/cetak'); ?>" method="POST">
+            <table>
+                <tr>
+                    <th colspan="3">
+                        Tampil Data Mata Kuliah
+                    </th>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                        <hr>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Kode MTK</th>
+                    <th>:</th>
+                    <td>
+                        <?= $kode ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Kode MTK</th>
+                    <th>:</th>
+                    <td>
+                        <?= $nama ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>SKS</th>
+                    <th>:</th>
+                    <td>
+                        <?= $sks ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3" align="center">
+                        <a href="<?= base_url('matakuliah') ?>">Kembali</a>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </center>
+</body>
+
+</html>
