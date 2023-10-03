ec0c191ed8754713f37e72d1847e160a89464b1d
GraciasKevinAmsalBramanta <graciaskevin99@gmail.com>
2023-10-03 20:28:22 +0700

    Update Pertemuan 4

---
+++ application/controllers/Web.php
@@ -0,0 +1,17 @@
+<?php
+
+class Web extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $data['judul'] = "Halaman Depan";
+        $this->load->view('v_header', $data);
+        $this->load->view('v_index', $data);
+        $this->load->view('v_footer', $data);
+    }
+}
